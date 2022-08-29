8060b1da0aae4b7c3fa238833d7d4265973ff163
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-08-29 14:28:39 -0400

    All odd number from 1-100

---
+++ src/homework/Main.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+        //All odd numbers from 1-100
+        for (int i=0; i<=100; i++){
+            if (i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
