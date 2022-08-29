2cba7a84ed216b9904552e2c2ece8fb40bac237f
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-08-29 14:47:53 -0400

    Code modified from odd to even numbers

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -8,5 +8,12 @@ public class Main {
                 System.out.print(i+" ");
             }
         }
+        System.out.println();
+        //Modifying code All even numbers from 1-100
+        for (int i=1; i<=100; i++){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+        }
     }
 }
